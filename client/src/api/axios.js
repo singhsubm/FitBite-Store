a7c02988c501f23eecd1d49f40ca9e94a795234a
True-Beast ‎ <singhsubm24@gamil.com>
2026-04-23 20:39:16 +0530

    loader added on dashboard

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  // baseURL: DevURL || BackendURL,
+  // baseURL: BackendURL,
 });
 
 // console.log("API URL =>", import.meta.env.VITE_API_URL);
